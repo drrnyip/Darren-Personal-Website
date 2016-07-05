7b7b38c9c3a92a620ed5c98d99a285009286e5c1
Darren <drrnyip@gmail.com>
2016-07-05 12:07:58 -0400

    second branch 2

---
+++ assets/js/base.js
@@ -0,0 +1,31 @@
+function goGitHub(){
+window.open("http://www.github.com/drrnyip");
+}
+
+function goLinkedIn(){
+window.open("https://www.linkedin.com/in/darren-yip-317b43ab?trk=nav_responsive_tab_profile");
+}
+
+function goFacebook(){
+window.open("https://www.facebook.com/drrnyip");
+}
+
+function mailTo(){
+window.location.href = "mailto:drrnyip@gmail.com";
+}
+
+function te1(){
+window.open("https://github.com/drrnyip/MontyRun");
+}
+
+function te2(){
+window.open("https://github.com/drrnyip/Hotel-Database");
+}
+
+function te3(){
+window.open("https://github.com/drrnyip/ChatApp");
+}
+
+function te4(){
+window.open("http://akashicrecordsusa.com/profamily2");
+}
